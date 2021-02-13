11c9ea4dd7d04fee4872abca64fe24ab0fb955dd
ouedraogoluc <ouedraogoluc664@gmail.com>
2021-02-13 01:45:52 +0100

    l'interface administrateurs

---
+++ AdminEspaceView/src/application/AdminHomeController.java
@@ -0,0 +1,62 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class AdminHomeController {
+
+    @FXML
+    private AnchorPane idAnchorpane;
+
+    @FXML
+    private VBox pnItems;
+
+    @FXML
+    private Button gAdmin;
+
+    @FXML
+    private Button gRespos;
+
+    @FXML
+    private Button gAssist;
+
+    @FXML
+    private Button gMembre;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    void Home(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleClckedAdmin(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleClckedAssist(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleClckedCreer(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleClckedMembre(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleClckedRespos(MouseEvent event) {
+
+    }
+
+}
